6a84433c2829885391674cf3adc2cc36d26170f8
Jamie Quigley <jamie@quigley.xyz>
2025-06-30 11:35:40 +0100

    use tempfile

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,37 @@
-use std::fs::File;
+use std::{fs::File, io::Read};
 use std::io::Write;
 use std::path::Path;
 use std::process::Command;
 
-use color_eyre::Result;
 use question::{Answer, Question};
 use tap::Tap;
 
-fn main() {
-    let r = real_main();
-    std::fs::remove_file("./.bulkrename").unwrap();
-    match r {
-        Ok(()) => {
-        }
-        Err(e) => {
-            eprintln!("{e}");
-            std::process::exit(1);
-        }
-    }
-}
-
-fn real_main() -> Result<()> {
-    color_eyre::install()?;
-
-    let tmp_file_name = Path::new("./.bulkrename");
-
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let names: Vec<_> = std::fs::read_dir(".")?
         .map(|x| x.map(|x| x.file_name().to_string_lossy().into_owned()))
         .collect::<Result<_, _>>()?;
 
-    if tmp_file_name.exists() {
-        std::fs::remove_file(tmp_file_name)?;
-    }
-
-    let mut file = File::create(tmp_file_name)?;
+    let mut file = tempfile::NamedTempFile::new()?;
 
     for name in &names {
         writeln!(&mut file, "{}", name)?;
     }
-    drop(file);
+
+    let file_path = file.into_temp_path();
 
     let editor = std::env::var("EDITOR")
         .unwrap_or_else(|_| std::env::var("VISUAL").unwrap_or_else(|_| "vim".to_string()));
     let mut editor_cmd = Command::new(editor);
-    editor_cmd.arg(tmp_file_name);
+    editor_cmd.arg(&file_path);
 
     editor_cmd.spawn()?.wait()?;
 
-    let renames = std::fs::read_to_string(tmp_file_name)?;
+    let renames = std::fs::read_to_string(file_path)?;
 
     let renames = names
         .iter()
         .map(|x| x.as_str())
-        .zip(renames.split('\n'))
+        .zip(renames.lines())
         .filter(|(name, new)| name != new)
         .collect::<Vec<_>>()
         .tap_mut(|x| x.sort_unstable());
